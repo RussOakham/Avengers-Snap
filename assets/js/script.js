9480dc60512b1c29adddb13b7c7fafd45d04e03a
RussOakham <rjoakham@gmail.com>
2021-03-02 20:08:30 +0000

    JS checkForMatch() disableCards() and unflipCards() scripts added

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,44 @@ function flipCard() {
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+    // detects if a card has been flipped and sets new card flip to 'second card', then checks if cards match via checkForMatch() function.
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+// Check if firstCard and secondCard flipped match, if true run disableCards(), if false run unflipCards().
+function checkForMatch() {
+    if (firstCard.dataset.avenger === secondCard.dataset.avenger) {
+        console.log('checked');
+        disableCards();
+        return;
+    }
+    unflipCards();    
+}
+
+// remove 'click' event listeners from matched cards, so can no longer to interacted with.
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    console.log('match');
+    console.log(firstCard.dataset.avenger);
+    console.log(secondCard.dataset.avenger);
+}
+
+// Unflip cards via removing 'flip' class from card. Function runs automatically after timeout expires, allowing user time to digest non-match.
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        console.log('non-match');
+        console.log(firstCard.dataset.avenger);
+        console.log(secondCard.dataset.avenger);
+    }, 1500);
 }
 
 
