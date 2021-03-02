8f1c360dc150f531b4e31625b342a7a5d23576b9
RussOakham <rjoakham@gmail.com>
2021-03-02 20:18:04 +0000

    lockBoard variable added - gitpod restart needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // Select all elements with class 'memory-card'
 const cards = document.querySelectorAll('.memory-card');
 
-// Variables to set all cards to not flipped and create first and second card variables
-let hasFlippedCard = false;
-let firstCard, secondCard;
+// Variables for script functionality
+let hasFlippedCard = false; `Variable to determine if card has been flipped or not`
+let lockBoard = false; `Variable to disable card click functions, until non-match cards unflip`
+let firstCard, secondCard; `Variables to determine first and second cards flipped for each match scenario`
 
-// Activate flip card animation on click, via adding 'flip' to class name and setting variable 'firstCard'.
+// Activate flip card animation on click, via adding 'flip' to class name and setting variable 'firstCard'. If lockBoard variable is true, function exits and no action in taken.
 function flipCard() {
+    if (lockBoard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -15,17 +17,16 @@ function flipCard() {
         return;
     }
 
-    // detects if a card has been flipped and sets new card flip to 'second card', then checks if cards match via checkForMatch() function.
+    // detects if a card has been flipped and sets new card flip to 'second card', then runs checkForMatch() function.
     secondCard = this;
     hasFlippedCard = false;
 
     checkForMatch();
 }
 
-// Check if firstCard and secondCard flipped match, if true run disableCards(), if false run unflipCards().
+// Check if firstCard and secondCard match, if true run disableCards(), if false run unflipCards().
 function checkForMatch() {
     if (firstCard.dataset.avenger === secondCard.dataset.avenger) {
-        console.log('checked');
         disableCards();
         return;
     }
@@ -36,19 +37,15 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-    console.log('match');
-    console.log(firstCard.dataset.avenger);
-    console.log(secondCard.dataset.avenger);
 }
 
 // Unflip cards via removing 'flip' class from card. Function runs automatically after timeout expires, allowing user time to digest non-match.
 function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-        console.log('non-match');
-        console.log(firstCard.dataset.avenger);
-        console.log(secondCard.dataset.avenger);
+        lockBoard = false;
     }, 1500);
 }
 
