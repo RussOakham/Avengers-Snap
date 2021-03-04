c2c17bac132a7421f4c64848da95ef47ccd4cd9c
RussOakham <rjoakham@gmail.com>
2021-03-04 14:48:53 +0000

    disabled ltp modal on page load until difficulty functions added

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,3 +1,5 @@
-$(window).on('load', function() {
-        $('#ltp-modal').modal('show');
-    });
+// Load how to play modal on window load.
+
+// $(window).on('load', function() {
+//         $('#ltp-modal').modal('show');
+//     });
