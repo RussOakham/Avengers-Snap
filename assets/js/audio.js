9e700573690deac87587ed77d9c4ca21b1e76a4f
RussOakham <rjoakham@gmail.com>
2021-03-08 13:13:03 +0000

    uploaded audio files, variables created in audio.js

---
+++ assets/js/audio.js
@@ -0,0 +1,6 @@
+// Global audio varaibles stored in below object for code cleanliness
+let audio = {
+    cardFlipAudio: new Audio("assets/audio/card-flip.mp3"),
+    cardMatchAudio: new Audio("assets/audio/card-match.mp3"),
+    cardNoMatchAudio: new Audio("assets/audio/card-no-match.mp3"),
+}
