f4a28d04e3317ee1d5a935d4c7ce5cfd78ec71f0
RussOakham <rjoakham@gmail.com>
2021-03-09 11:53:08 +0000

    removed sound mute toggle until properly scripted

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -27,16 +27,6 @@ function defaultVolume() {
     audio.cardNoMatchAudio.volume = audio.soundVolumeSlider.defaultValue / 100;
 }
 
-// Function to set audio to to zero if sound toggle is turned to off
-function soundMute() {
-    audio.cardFlipAudio.volume = 0;
-    audio.cardMatchAudio.volume = 0;
-    audio.cardNoMatchAudio.volume = 0;
-    audio.soundVolumeSlider.value = 0;
-    console.log("audio muted");
-}
-
-
 // Event Listeners
 // Event listener records change in volume slider input
 audio.soundVolumeSlider.addEventListener('change', event => {
@@ -44,5 +34,3 @@ audio.soundVolumeSlider.addEventListener('change', event => {
     audio.cardMatchAudio.volume = audio.soundVolumeSlider.value / 100;
     audio.cardNoMatchAudio.volume = audio.soundVolumeSlider.value / 100;
 });
-
-$('#sound-toggle').change(soundMute());
