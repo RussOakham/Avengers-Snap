cbcd10da765a6d0e7b01e4666f287b98a5dbe42c
RussOakham <rjoakham@gmail.com>
2021-03-15 11:42:07 +0000

    Audio Mute - script added and tested

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -5,19 +5,32 @@ let audio = {
     cardNoMatchAudio: new Audio("assets/audio/card-no-match.mp3"),
     soundVolumeSlider: document.getElementById("volume-slider"),
     soundOnOff: document.getElementById("sound-toggle"),
+    soundMute: false,
 }
 
 // 3x Sound Functions used to play audio effects
 function cardFlipSound() {
-    audio.cardFlipAudio.play();
+    if (audio.soundMute === true) {
+        return;
+    } else {
+        audio.cardFlipAudio.play();
+    }
 }
 
 function cardMatchSound() {
-    audio.cardMatchAudio.play();
+    if (audio.soundMute === true) {
+        return;
+    } else {
+        audio.cardMatchAudio.play();
+    }
 }
 
 function cardNoMatchSound() {
-    audio.cardNoMatchAudio.play();
+    if (audio.soundMute === true) {
+        return;
+    } else {
+        audio.cardNoMatchAudio.play();
+    }
 }
 
 // Function sets default sound effect volume on game load
@@ -27,6 +40,16 @@ function defaultVolume() {
     audio.cardNoMatchAudio.volume = audio.soundVolumeSlider.defaultValue / 100;
 }
 
+function audioMute() {
+    if (audio.soundMute === false) {
+        audio.soundMute = true;
+        console.log(audio.soundMute);
+    } else if (audio.soundMute === true) {
+        audio.soundMute = false;
+        console.log(audio.soundMute);
+    }
+}
+
 // Event Listeners
 // Event listener records change in volume slider input
 audio.soundVolumeSlider.addEventListener('change', event => {
@@ -34,3 +57,10 @@ audio.soundVolumeSlider.addEventListener('change', event => {
     audio.cardMatchAudio.volume = audio.soundVolumeSlider.value / 100;
     audio.cardNoMatchAudio.volume = audio.soundVolumeSlider.value / 100;
 });
+
+// Event listener for Audio mute toggle - function from bootstraptoggle.com
+$(function () {
+    $('#sound-toggle').change(function () {
+        audioMute()
+    })
+})
